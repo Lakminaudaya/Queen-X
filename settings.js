a883a3fa0ad84d34feef4a574b22412bc64a89d7
Lakiya official <93707390+Lakminaudaya@users.noreply.github.com>
2023-02-16 11:58:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
-global.ownername = "©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
-global.ytname = "ʏᴜʀᴇꜱʜ" //ur yt chanel name
-global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or insta name
+global.owner = ['94784608102'] //ur owner number
+global.ownername = "© 𝚂𝚈𝙱𝙴𝚁 𝙱𝙻𝙰𝙲𝙺 𝙰𝙻𝙿𝙰𝙷" //ur owner name
+global.ytname = "𝚂𝙰𝙲𝙷𝙸𝙽𝚃𝙷𝙰" //ur yt chanel name
+global.socialm = "🍃〽️𝚂𝚈𝙱𝙴𝚁 𝙱𝙻𝙰𝙲𝙺 𝙰𝙻𝙿𝙰𝙷〽️🍃" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
-global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
-global.websitex = "https:// no" //ur website to be displayed
+global.ownernomer = "94784608102" //ur number
+global.premium = ['94784608102'] //ur premium number
+global.botname = '© 𝚂𝚈𝙱𝙴𝚁 𝙱𝙻𝙰𝙲𝙺 𝙰𝙻𝙿𝙰𝙷' //ur bot name
+global.linkz = "https://chat.whatsapp.com/ILMuX8YOc8TGolDYvJRlk5" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/@RDXMODSOFFICIAL" //ur website to be displayed
 global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
 global.themeemoji = "🛸" //ur theme emoji
-global.packname = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark packname
-global.author = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
-global.wm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻." //ur watermark
+global.packname = "🍃〽️𝚂𝚈𝙱𝙴𝚁 𝙱𝙻𝙰𝙲𝙺 𝙰𝙻𝙿𝙰𝙷〽️🍃" //ur sticker watermark packname
+global.author = "🍃〽️𝚂𝚈𝙱𝙴𝚁 𝙱𝙻𝙰𝙲𝙺 𝙰𝙻𝙿𝙰𝙷〽️🍃\n\n\nA whatsapp bot developed by\n𝙻𝙰𝙺𝙸𝚈𝙰" //ur sticker watermark author
+global.wm = "🍃〽️𝚂𝚈𝙱𝙴𝚁 𝙱𝙻𝙰𝙲𝙺 𝙰𝙻𝙿𝙰𝙷〽️🍃" //ur watermark
 
 // Other
 global.sessionName = 'session'
